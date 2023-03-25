12ac27412dae219dfe946c2cd0c78e430728176d
niladrimondal <niladrimondal@users.noreply.github.com>
2023-03-25 16:24:30 +0530

    Create creds.tf

---
+++ creds.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  access_key = "ASIAQYBHK4LR76ZYH6UN"
+  secret_key = "XR3K1FPOxfPSED2nqt0Zsuk+i9Gne6w+BjUakBnL"
+  token = "FwoGZXIvYXdzEC0aDLSmn/iKur2HiCTxECK/Aey3poAlL5HUdmCsj9O0YFOjnmDpTkP24yAUzTcqCb21VDgzB6GeR3PES2B1oTdxK4A9fSEkiutfKqqqJCQxbdIYvtAuVqma7rBvnmtZ61CAw+I0okKicw/wr82s4YDUOnVz2knwtNvX/QvmZGSQwxmuk4Ku21+NuLx24UcbyLz/JwWDAhv1zyTIEvLw1pBoSX9nJj761ycN2nchcNqHX/ztjYVg7yueOMh9ansWPuC+oAw2GJF15qxiuClwqPUtKPfj+aAGMi2E4ZSSyquXFMK7n8c/+QTSdeHHnzx+fri835k7QRRyb2g7aQqy+AK3+Q9IBsU="
+  region = "us-east-1"
+}
